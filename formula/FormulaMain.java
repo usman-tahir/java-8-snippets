3b3b4dd58bad5fe3386c084d2fe689bd3d6fb975
usman-tahir <tahir.usman.ali94@outlook.com>
2017-09-26 13:13:36 -0400

    feat: added an example that uses a default method for an interface

---
+++ formula/FormulaMain.java
@@ -0,0 +1,14 @@
+
+public class FormulaMain {
+	public static void main(String[] args) {
+		Formula formula = new Formula() {
+			@Override
+			public double calculate(int a) {
+				return sqrt(a * 100);
+			}
+		};
+
+		System.out.println(formula.calculate(100));
+		System.out.println(formula.sqrt(16));
+	}
+}
